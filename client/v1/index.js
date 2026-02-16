5f005d3b8e94d06c2c669de1ef1b1a1097fbf406
charles ayoub <charles.ayoub@edu.devinci.fr>
2026-02-16 12:43:41 +0100

    TODO6

--- client/v1/index.js
+++ client/v1/index.js
@@ -105,6 +105,8 @@ console.log(dealsByDate);
 // 1. Filter the list of deals between 50% and 75%
 // 2. Log the list
 
+const reduction_sorted=deals.filter(a=>a.discount>50 && a.discount<75)
+console.log(reduction_sorted)
 // 🎯 TODO 7: Average percentage discount
 // 1. Determine the average percentage discount of the deals
 // 2. Log the average
